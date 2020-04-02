98b66ed49a2fff4240de5d88763f96c5e1468aca
juandiego93 <juand.osorio@hotmail.com>
2020-04-01 19:25:54 -0500

    Componentes y modulos manual

---
+++ proyectos/holaMundo1/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre: string = 'Juan Diego';
+  apellido: string = 'Osorio Castrillon';
+
+}
