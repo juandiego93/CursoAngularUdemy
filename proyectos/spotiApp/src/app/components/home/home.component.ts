8f899a865ced9982074c0942fe1bc84348fed323
juandiego93 <juand.osorio@hotmail.com>
2020-04-26 16:25:16 -0500

    finish api apotify

--- proyectos/spotiApp/src/app/components/home/home.component.ts
+++ proyectos/spotiApp/src/app/components/home/home.component.ts
@@ -13,8 +13,11 @@ export class HomeComponent implements OnInit {
   countries: any[] = [];
   newReleases: any[] = [];
   loading: boolean;
+  error: boolean;
+  messageError: string;
 
   constructor(private http: HttpClient, private spotify: SpotifyService) {
+    this.error = false;
     this.loading = true;
     this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe((countries: any) => {
       this.countries = countries;
@@ -22,6 +25,10 @@ export class HomeComponent implements OnInit {
     this.spotify.getNewReleases().subscribe((data: any) => {
       this.newReleases = data;
       this.loading = false;
+    }, (errorServices) => {
+      this.error = true;
+      this.loading = false;
+      this.messageError = errorServices.error.error.message;
     });
   }
 
