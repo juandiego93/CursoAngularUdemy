90bec5ec8564088aefd48463b08606a403c8271b
juandiego93 <juand.osorio@hotmail.com>
2020-04-26 21:42:09 -0500

    init app ionic

---
+++ proyectos/myApp/src/app/models/lista.model.ts
@@ -0,0 +1,19 @@
+import { ListaItem } from './lista.items.model';
+
+export class Lista {
+	id: number;
+	titulo: string;
+	creadoEn: Date;
+	terminadaEn: Date;
+	terminada: boolean;
+	items: ListaItem[];
+
+	constructor(titulo: string) {
+		this.titulo = titulo;
+		this.creadoEn = new Date();
+		this.terminada = false;
+		this.items = [];
+		this.id = new Date().getTime();
+	}
+
+}
