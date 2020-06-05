965c6051b3fad6ddefb6b22027c3ad4f6fafdd41
juandiego93 <juand.osorio@hotmail.com>
2020-06-05 00:50:24 -0500

    finish app with Auth0

--- proyectos/authApp/src/app/app.component.ts
+++ proyectos/authApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
-  ngOnInit(){ 
+  ngOnInit() {
+    this.auth.localAuthSetup()
   }
 }
