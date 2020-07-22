9f71b8406abfd4cf00dd82ae1b8894f42e3c25c8
Juan Diego Ariadna <diego.osorio@ariadnacg.com>
2020-07-22 00:15:07 -0500

    Init project Forms Angular

---
+++ proyectos/formularios/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent,
+    ReactiveComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
