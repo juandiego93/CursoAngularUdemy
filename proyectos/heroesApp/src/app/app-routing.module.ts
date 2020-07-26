ad2b95f03d74e6ccc53abe2d97b8e0654bb06903
Juan Diego Ariadna <diego.osorio@ariadnacg.com>
2020-07-26 00:51:12 -0500

    Continue course Udemy

--- proyectos/heroesApp/src/app/app-routing.module.ts
+++ proyectos/heroesApp/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 
 
+const ROUTES: Routes = [
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' },
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(ROUTES)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
